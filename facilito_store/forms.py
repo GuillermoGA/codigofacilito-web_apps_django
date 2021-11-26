b385012cae4972a49ca4b7a907d9ff8111f9aa56
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-11-26 22:08:36 +0100

    validate fields

--- facilito_store/forms.py
+++ facilito_store/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class RegisterForm(forms.Form):
     username = forms.CharField(
@@ -25,3 +26,19 @@ class RegisterForm(forms.Form):
             'class': 'form-control'
         })
     )
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('El username ya se encuentra en uso')
+        
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('El email ya se encuentra en uso')
+        
+        return email
