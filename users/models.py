91ab471c733e086fd1028ad9cd77feb4f7408e0c
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-11-28 23:29:39 +0100

    create a proxy model Customer from User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Customer(User):
+    class Meta:
+        proxy = True
+
+    def get_product(self):
+        return []
