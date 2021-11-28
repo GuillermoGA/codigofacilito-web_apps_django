df47c67096529cb847971d998c88068ca8aaad50
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-11-28 16:54:02 +0100

    search products and list them

--- products/views.py
+++ products/views.py
@@ -18,3 +18,19 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'products/product.html'
+
+class ProductSearchListView(ListView):
+    template_name = 'products/search.html'
+
+    def get_queryset(self):
+        # SELECT * FROM Products WHERE title LIKE %query%
+        return Product.objects.filter(title__icontains=self.query())
+
+    def query(self):
+        return self.request.GET.get('q')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.query()
+        context['count'] = context['product_list'].count()
+        return context
