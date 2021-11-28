9a2f797ac7bda71ce318993c6e14842cd8373e16
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-11-28 23:39:39 +0100

    extend User with more fields via new class model and OneToOneField

--- users/models.py
+++ users/models.py
@@ -8,3 +8,7 @@ class Customer(User):
 
     def get_product(self):
         return []
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
