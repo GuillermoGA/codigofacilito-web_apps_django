488c2368605384488e1777f5acc666529d48ecde
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-12-11 16:45:06 +0100

    fix bug

--- carts/utils.py
+++ carts/utils.py
@@ -3,7 +3,7 @@ from .models import Cart
 
 def get_or_create_cart(request):
     user = request.user if request.user.is_authenticated else None
-    cart_id = request.sesion.get('cart_id')
+    cart_id = request.session.get('cart_id')
 
     cart = Cart.objects.filter(cart_id=cart_id).first()
     if cart is None:
