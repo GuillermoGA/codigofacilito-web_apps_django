261b1649f9e3f9c8e34a16076edb7d832de43bd9
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-12-12 17:00:56 +0100

    add multiple products with quantity to cart

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
+from .models import CartProducts
 from products.models import Product
 from .utils import get_or_create_cart
 
@@ -15,9 +16,20 @@ def cart(request):
 def add(request):
     cart = get_or_create_cart(request)
     product = get_object_or_404(Product, pk=request.POST.get('product_id'))
-    cart.products.add(product)
+    quantity = int(request.POST.get('quantity', 1))
+
+    # cart.products.add(product, through_defaults={
+    #     'quantity': quantity
+    # })
+
+    cart_product = CartProducts.objects.get_or_update_quantity(
+        cart=cart,
+        product=product,
+        quantity=quantity
+    )
 
     return render(request, 'carts/add.html', {
+        'quantity': quantity,
         'product': product
     })
 
