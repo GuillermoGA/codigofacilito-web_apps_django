fe2fdf5decf6781eac731fc78dbe2ce53bb8e477
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-12-19 16:59:30 +0100

    get or create order

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from carts.utils import get_or_create_cart
+from orders.utils import get_or_create_order
+
+
 def order(request):
+    cart = get_or_create_cart(request)
+    order = get_or_create_order(cart, request)
+
     return render(request, 'orders/order.html', {
 
     })
