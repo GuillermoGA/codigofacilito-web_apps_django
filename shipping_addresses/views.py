20f1b02cf457816fe3ed0f817bcc838a91b3ec05
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-12-20 18:52:46 +0100

    WIP create shipping list view

--- shipping_addresses/views.py
+++ shipping_addresses/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from shipping_addresses.models import ShippingAddress
+
+
+class ShippingAddressListView(ListView):
+    model = ShippingAddress
+    template_name = 'shipping_addresses/shipping_addresses.html'
+
+    def get_queryset(self):
+        return ShippingAddress.objects.filter(user=self.request.user).order_by('-default')
