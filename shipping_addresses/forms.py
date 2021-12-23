beae0966970fef03d82a452dfee8ce967a34205e
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-12-23 13:24:00 +0100

    add style to ModelForm

--- shipping_addresses/forms.py
+++ shipping_addresses/forms.py
@@ -15,3 +15,27 @@ class ShippingAddressForm(ModelForm):
             'postal_code',
             'reference',
         ]
+
+        labels = {
+            'line1': "Calle 1",
+            'line2': "Calle 2",
+            'city': "Ciudad",
+            'state': "Estado",
+            'country': "País",
+            'postal_code': "Codigo postal",
+            'reference': "Referencias"
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Common style
+        for field_data in self.fields.values():
+            field_data.widget.attrs.update({
+                "class": "form-control"
+            })
+
+        # Individual style
+        self.fields["postal_code"].widget.attrs.update({
+            "placeholder": "0000"
+        })
