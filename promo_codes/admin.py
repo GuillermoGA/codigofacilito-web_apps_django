8fa8af871018cf55d761255c7697db72dc0eb3ea
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2022-01-21 14:42:01 +0100

    autocreate promo code + validate promo code

--- promo_codes/admin.py
+++ promo_codes/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import PromoCode
 
-admin.site.register(PromoCode)
+
+class PromoCodeAdmin(admin.ModelAdmin):
+    exclude = ["code"]
+
+
+admin.site.register(PromoCode, PromoCodeAdmin)
