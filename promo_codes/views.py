301d5de216edc89d94f6f59c5e8ca8fd19f3c6a4
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2022-01-21 15:55:38 +0100

    use validate and update order with promo code

--- promo_codes/views.py
+++ promo_codes/views.py
@@ -1,9 +1,11 @@
 from django.http import JsonResponse
 
+from orders.decorators import validate_cart_and_order
 from promo_codes.models import PromoCode
 
 
-def validate(request):
+@validate_cart_and_order
+def validate(request, cart, order):
     code = request.GET.get("code")
     promo_code = PromoCode.objects.filter(code=code).first()
 
@@ -12,8 +14,11 @@ def validate(request):
             "status": False
         }, status=404)
 
+    order.apply_promo_code(promo_code)
+
     return JsonResponse({
         "status": True,
         "code": promo_code.code,
-        "discount": promo_code.discount
+        "discount": promo_code.discount,
+        "total": order.total
     })
