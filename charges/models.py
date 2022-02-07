20bfaf378680bbd3cd7472551dcd30f91b2091c9
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2022-02-07 20:17:25 +0100

    create charges app, model and register it

---
+++ charges/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from orders.models import Order
+from users.models import User
+
+
+class Charge(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    charge_id = models.CharField(max_length=50)
+    amount = models.IntegerField()
+    payment_method = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.charge_id
